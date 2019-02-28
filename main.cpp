c6b02e20c7734158d92c6ed5a0241804c1e0b3b6
ligang945 <ligang945@users.noreply.github.com>
2019-02-28 10:30:58 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 /** @author ligang
   * @date 20190228
 **/
+
 #include "stdio.h"
 #include "tinyxml2.h"
 
-using namespace std;
 using namespace tinyxml2;
 
 int main(int argc, char *argv[])
 {
     XMLDocument doc;
-	doc.LoadFile(argv[1]);
+    doc.LoadFile(argv[1]);
     doc.SaveFile(argv[1]);
 
     return 0;
